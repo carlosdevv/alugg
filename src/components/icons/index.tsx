60bd81a8464815cec5858baebbb236722ce57844
carlosdevv <carloslopessf@gmail.com>
2024-08-21 11:18:55 -0300

    feat: navbar

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,42 @@
+import {
+  CalendarDaysIcon,
+  CheckCircle2Icon,
+  CheckIcon,
+  ChevronDownIcon,
+  CircleHelpIcon,
+  CircleIcon,
+  CircleMinusIcon,
+  CirclePlusIcon,
+  Clock1Icon,
+  EllipsisIcon,
+  Home,
+  LoaderIcon,
+  Package,
+  Settings2,
+  ShoppingCart,
+  SlashIcon,
+  Trash2Icon,
+  type LucideIcon
+} from "lucide-react";
+
+export type IconType = LucideIcon;
+
+export const Icons = {
+  package: Package,
+  home: Home,
+  shoppingCart: ShoppingCart,
+  circlePlus: CirclePlusIcon,
+  circleMinus: CircleMinusIcon,
+  help: CircleHelpIcon,
+  slash: SlashIcon,
+  loader: LoaderIcon,
+  check: CheckIcon,
+  arrowDown: ChevronDownIcon,
+  calendar: CalendarDaysIcon,
+  horizontalEllipsis: EllipsisIcon,
+  delete: Trash2Icon,
+  settings: Settings2,
+  circle: CircleIcon,
+  clock: Clock1Icon,
+  checkCircle: CheckCircle2Icon,
+};
